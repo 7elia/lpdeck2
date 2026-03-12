04f8f156f5f56f41da381cca319fb58e489e01cf
7elia <176603882+7elia@users.noreply.github.com>
2026-03-12 23:22:11 +0100

    downgrade to 0.15.2

--- src/main.zig
+++ src/main.zig
@@ -5,24 +5,28 @@ const midi = @import("midi.zig");
 
 var running = std.atomic.Value(bool).init(true);
 
-fn sigintHandler(sig: posix.SIG) callconv(.c) void {
+fn sigintHandler(sig: i32) callconv(.c) void {
     _ = sig;
     running.store(false, .release);
 }
 
-pub fn main(init: std.process.Init) !void {
+pub fn main() !void {
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
     const act = posix.Sigaction{
         .handler = .{ .handler = sigintHandler },
         .flags = 0,
-        .mask = posix.sigemptyset(),
+        .mask = std.mem.zeroes(posix.sigset_t),
     };
     posix.sigaction(posix.SIG.INT, &act, null);
 
-    const port_opt = try midi.MidiPort.init_with_name(init.gpa, "Launchpad");
+    const port_opt = try midi.MidiPort.init_with_name(alloc, "Launchpad");
     if (port_opt) |port| {
         defer port.deinit();
 
-        var client = try lp.LaunchpadClient.open(init.gpa, port.full_name);
+        var client = try lp.LaunchpadClient.open(alloc, port.full_name);
         defer client.deinit();
 
         try client.set_color(
